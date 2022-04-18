a09a83696316b99da86ae4bf8c9b9ddb30b97a00
bharatsavanur11 <bharat.savanur@gmail.com>
2022-04-18 13:41:46 +0530

    Adding Spring Sleuth Dependency

--- gm-booking-service/src/main/java/gmbooking/gmbookingservice/GMBookingRest.java
+++ gm-booking-service/src/main/java/gmbooking/gmbookingservice/GMBookingRest.java
@@ -1,5 +1,7 @@
 package gmbooking.gmbookingservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GMBookingRest {
 
+    private static Logger log = LoggerFactory.getLogger(GMBookingRest.class);
+
+
     @Value("${msg:Default}")
     private String msg;
 
     @GetMapping("/message")
     public String getMessage() {
+        log.info("Enabling tracer and span id");
         return this.msg;
     }
 }
